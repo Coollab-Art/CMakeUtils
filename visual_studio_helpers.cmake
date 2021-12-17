74ffae56a36a538bfb9d075c7be4f1fccfdcdf38
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-17 13:31:45 +0100

    [visual_sutdio_helpers] Cleanup

--- visual_studio_helpers.cmake
+++ visual_studio_helpers.cmake
@@ -1,8 +1,9 @@
-# Hide "ALL_BUILD" and "ZERO_CHECK" in a folder
-set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+function(Cool__vs_hide_all_build_and_zero_check)
+    set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+endfunction()
 
-# Recreate the folder architecture inside the Visual Studio solution (might work for other IDEs as well)
-function(VS_RegisterFiles _FILES)
+# Recreate the folder architecture inside the Visual Studio solution
+function(Cool__vs_register_files _FILES)
     foreach(_file IN ITEMS ${_FILES})
         get_filename_component(_filepath "${_file}" PATH)
         string(REPLACE "${CMAKE_SOURCE_DIR}" "" _group_path "${_filepath}")
